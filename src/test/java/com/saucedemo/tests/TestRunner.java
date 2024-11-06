95470a29451004fb00d6fd6afe5e65169dc1fea1
AbdullahAmer93 <abdullah.amer1@vodafone.com>
2024-11-07 01:41:33 +0200

    refactoring testrunner

--- src/test/java/com/saucedemo/tests/TestRunner.java
+++ src/test/java/com/saucedemo/tests/TestRunner.java
@@ -1,15 +1,22 @@
 package com.saucedemo.tests;
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.platform.suite.api.SelectClasspathResource;
-import org.junit.platform.suite.api.Suite;
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features"
+        ,glue={"com.saucedemo.StepDefinition"},    // Path to the step definition classes
+        plugin = {
+                "pretty",  // Prints the output of the test to the console in a readable format
+                "html:target/cucumber-reports/cucumber.html",  // Generates an HTML report
+                "json:target/cucumber-reports/cucumber.json"   // Generates a JSON report
+        },
+        monochrome = true,  // Ensures that console output is more readable (no escape characters)
+        tags = "@SmokeTest",  // If you want to run tests with a specific tag, for example, @SmokeTest
+        strict = true
+)
 
-@Suite
-@SelectClasspathResource("features")
 public class TestRunner {
 
-    @BeforeAll
-    public static void setup() {
-
-    }
 }
